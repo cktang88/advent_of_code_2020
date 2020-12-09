3ce64bad43e3d0872600cc8c8271243b292d2a31
Kwuang Tang <10319942+cktang88@users.noreply.github.com>
2020-12-09 11:28:31 -0600

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,27 +1,13 @@
-import re
-import pprint
 inp = open('9.txt','r')
 arr = [int(a.strip()) for a in inp]
-'''
-part 1
-'''
+
 for m in range(25,len(arr)-1):
   sub = arr[m-25:m]
-  good=False
-  for i in sub:
-    for j in sub:
-      if i == j:
-        continue
-      if i+j == arr[m]:
-        good=True
-        break
+  good=any(i!=j and i+j == arr[m] for i in sub for j in sub)
   if not good:
     print(arr[m], m)
     break
-'''
-part 2
-'''
-# from part 1
+
 tot = 31161678
 n = 509
 for i in range(0, n):
