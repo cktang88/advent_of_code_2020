bd52c870232b49364738952c51a0fb3fed0bf74a
Kwuang Tang <10319942+cktang88@users.noreply.github.com>
2020-12-09 11:30:11 -0600

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,17 +1,13 @@
 inp = open('9.txt','r')
 arr = [int(a.strip()) for a in inp]
-
 for m in range(25,len(arr)-1):
   sub = arr[m-25:m]
-  good=any(i!=j and i+j == arr[m] for i in sub for j in sub)
-  if not good:
+  if not any(i!=j and i+j == arr[m] for i in sub for j in sub):
     print(arr[m], m)
     break
-
-tot = 31161678
-n = 509
-for i in range(0, n):
-  for j in range(i+2, n):
+ind, tot = 509, 31161678
+for i in range(0, ind):
+  for j in range(i+2, ind):
     if sum(arr[i:j]) == tot:
       print(min(arr[i:j]) + max(arr[i:j]))
       break
