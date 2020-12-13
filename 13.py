a84c0b98a7ff413617e2a42cbd4cf468417ae06f
Kwuang Tang <10319942+cktang88@users.noreply.github.com>
2020-12-13 15:37:23 -0600

    Update 13.py

--- 13.py
+++ 13.py
@@ -2,7 +2,7 @@ import re
 import pprint
 from copy import deepcopy
 
-inp = open('13b.txt','r')
+inp = open('13.txt','r')
 arr = [a.strip() for a in inp]
 pprint.pprint(arr)
 start = int(arr[0])
@@ -13,68 +13,47 @@ for i,b in enumerate(arr[1].split(',')):
   buses.append((i%int(b),int(b)))
 print(buses)
 
-
-
+'''
+part 2
+'''
+from functools import reduce
+def _lcm(arr):
+  gcd = lambda a,b: a if b==0 else gcd(b, a%b)
+  return reduce(lambda x,y: x*y//gcd(x, y), arr)
 
 def solve(bus, mult):
-  # solve: hop * x + rem = 0 (mod mult)
+  # solve: hop * x - rem = 0 (mod mult)
   rem, hop = bus
-  for i in range(1, max(hop, mult)*10):
-    if (hop * i - rem)%mult == 0:
+  for i in range(1, max(hop, mult)*20):
+    # print(i, (hop * i + rem)%mult)
+    if (hop * i + rem)%mult == 0:
       return i
 
 def shift(oldtracks):
   shift = oldtracks[0][0]
   return shift, [((t[0] - shift)%t[1], t[1]) for t in oldtracks]
 
-oldtracks = buses
-TOTAL_SHIFT = 0
-
-tracks = []
+oldtracks, tracks, TOTAL_SHIFT = buses, [], 0
 while True:
   orig = oldtracks[0][1]
-  oldtracks = oldtracks[1:]
-  tracks = []
+  oldtracks, tracks = oldtracks[1:], []
   for bus in oldtracks:
     N = solve(bus, orig)
-    start = N*bus[1] - bus[0]
-    print(bus, N, start//orig)
-    hop = bus[1] * orig
+    start = N*bus[1] + bus[0]
+    # print(bus, N, start//orig)
+    hop = _lcm([bus[1], orig])
     tracks.append((start, hop))
   if len(tracks) > 1:
-    print(tracks)
+    # print(tracks)
     addShift, tracks = shift(tracks)
     TOTAL_SHIFT += addShift
-    print('shifted', tracks)
+    # print('shifted', tracks)
     oldtracks = tracks
   else:
     break
 
 print(tracks, TOTAL_SHIFT)
-# start, hop = max(tracks, key=lambda x: x[1])
-# print(start, hop)
-# for i in range(100000000000000,150000000000000,orig):
-_max = 150000000000
-hop = tracks[0][0]
-for i in range(hop,_max, hop):
-  print(hop - TOTAL_SHIFT)
-  break
-
-# _max = 150000000000000
-# for cur in range(start, _max, hop):
-#   if all((cur-rem)%hop == 0 for (rem, hop) in tracks):
-#     print(cur, TOTAL_SHIFT)
-#     break
-  # print(i)
-# for rem,b in buses:
-#   print(b-100001941136560%b, rem)
-
-# from functools import reduce
-# gcd = lambda a,b: a if b==0 else gcd(b, a%b)
-# def _lcm(lst):
-#     return reduce(lambda x,y: x*y//gcd(x, y), lst)  
-# lcm = _lcm([14, 26])
-# print('lcm', lcm)
+print(tracks[0][1] - tracks[0][0]-TOTAL_SHIFT)
 
 '''
 part 1
